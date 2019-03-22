b53a34bd39c8a615024a97e3fd0ad4f6858cea7c
cdlab-at-bu <cdlab.at.bu@gmail.com>
2019-03-22 16:46:34 -0400

    new

--- timing task/TimingTask_lastrun.py
+++ timing task/TimingTask_lastrun.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 This experiment was created using PsychoPy3 Experiment Builder (v3.0.4),
-    on Thu Mar 21 16:05:22 2019
+    on Fri Mar 22 16:04:06 2019
 If you publish work using this script please cite the PsychoPy publications:
     Peirce, JW (2007) PsychoPy - Psychophysics software in Python.
         Journal of Neuroscience Methods, 162(1-2), 8-13.
